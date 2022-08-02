52110e13d3886c29640e926858cdb1dd79487f24
Lucas Marques <lucasmarquesds@gmail.com>
2022-08-01 22:19:11 -0300

    feat: add prototype pattern

--- app/design_patterns/creational/prototype.py
+++ app/design_patterns/creational/prototype.py
@@ -4,3 +4,18 @@ Prototype:
     
 See: https://refactoring.guru/design-patterns/prototype
 """
+from __future__ import annotations  # enables using current class as returning type
+import copy
+
+
+class StackOverflowAnswer:
+    def __init__(self, code: str, is_correct: bool) -> None:
+        self.code = code
+        self.is_correct = is_correct
+
+    def __clone__(self) -> StackOverflowAnswer:
+        new_code = copy.copy(self.code)
+        new_is_correct = copy.copy(self.is_correct)
+
+        new_answer = self.__class__(new_code, new_is_correct)
+        return new_answer
--- tests/design_patterns/creational/test_prototype.py
+++ tests/design_patterns/creational/test_prototype.py
@@ -1,2 +1,11 @@
+import copy
+from app.design_patterns.creational.prototype import StackOverflowAnswer
+
+
 class TestPrototype:
-    pass
+    def test_should_clone_object_with_all_its_attributes(self):
+        expected_object = StackOverflowAnswer("<h1>this answer is right</h1>", True)
+        result_object = copy.copy(expected_object)
+
+        assert expected_object.code == result_object.code
+        assert expected_object.is_correct == result_object.is_correct
